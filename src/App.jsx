92360f058e308774682f78c1033a220f4bed8f4f
PushEax <pusheax.vk@gmail.com>
2025-04-20 09:29:02 +0300

    test include modules

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import toast, { Toaster } from 'react-hot-toast';
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
-const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage/MovieDetailsPage'));
+const MovieDetailsPage = lazy(() =>
+  import('./pages/MovieDetailsPage/MovieDetailsPage')
+);
 const MovieCast = lazy(() => import('./components/MovieCast/MovieCast'));
 
 function App() {
@@ -13,6 +15,9 @@ function App() {
       <div className={css.app}>
         <h1>Hello from React!</h1>
         <HomePage />
+        <MoviesPage />
+        <MovieDetailsPage />
+        <MovieCast />
         <Toaster position="bottom-right" />
       </div>
     </Suspense>
