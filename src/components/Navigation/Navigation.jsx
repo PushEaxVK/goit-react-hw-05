a54f0a3d496d15a48882f0fd5c5d0d8e88c62e1f
PushEax <pusheax.vk@gmail.com>
2025-04-21 16:08:05 +0300

    add transition effect

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,21 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import s from './Navigation.module.css';
+import clsx from 'clsx';
 
 const Navigation = () => {
+  const setActiveClass = ({ isActive }) => {
+    return clsx(s.link, isActive && s.active);
+  };
+
   return (
     <header className={s.navigation}>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/movies/">Movies</Link>
+      <nav className={s.menu}>
+        <NavLink className={setActiveClass} to="/">
+          Home
+        </NavLink>
+        <NavLink className={setActiveClass} to="/movies/">
+          Movies
+        </NavLink>
       </nav>
     </header>
   );
