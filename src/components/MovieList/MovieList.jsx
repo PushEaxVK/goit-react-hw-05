c66b02694b6bb6a14e5296f4f1cb09501a19c278
PushEax <pusheax.vk@gmail.com>
2025-04-21 23:31:21 +0300

    movie list

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,7 +1,24 @@
+import { Link, useLocation } from 'react-router-dom';
 import s from './MovieList.module.css';
 
-const MovieList = () => {
-  return <div className={s.movieList}>MovieList component</div>;
+const MovieList = ({ movies }) => {
+  const location = useLocation();
+
+  return (
+    <div className={s.movieList}>
+      {movies && movies.length > 0 && (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <Link to={`/movies/${movie.id}`} state={location}>
+                {movie.original_title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default MovieList;
