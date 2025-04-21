476c3e1f4dd8e9146ffd2ec26b3585df6a87c6a7
PushEax <pusheax.vk@gmail.com>
2025-04-21 23:31:11 +0300

    movie details page

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,7 +1,74 @@
+import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 import s from './MovieDetailsPage.module.css';
+import { useEffect, useState } from 'react';
+import { getDetails, cancelDetailsRequest } from '../../services/api';
+import toast from 'react-hot-toast';
+import { useApp } from '../../appContext';
 
 const MovieDetailsPage = () => {
-  return <div className={s.detail}>MovieDetailsPage page</div>;
+  const { movieId } = useParams();
+  const [details, setDetails] = useState(null);
+  const location = useLocation();
+  const { config } = useApp();
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        const detailsData = await getDetails(movieId);
+        if (detailsData) {
+          console.log(detailsData);
+          setDetails(detailsData);
+        }
+      } catch (error) {
+        console.error(error);
+        toast.error('Error when loading Movie Detail!');
+      }
+    };
+    fetchDetails();
+    return () => cancelDetailsRequest();
+  }, [movieId, setDetails]);
+
+  return (
+    <div className={s.detail}>
+      <Link to={location.state} className={s.back}>
+        Go back
+      </Link>
+      {details && config && (
+        <>
+          <div className={s.thumb}>
+            <img
+              src={`${config.images.secure_base_url}w500${details.poster_path}`}
+              alt={details.title}
+              width="250"
+              height="375"
+            />
+            <div className={s.detailsText}>
+              <h2 className={s.title}>
+                {details.title} ({details.release_date.slice(0, 4)})
+              </h2>
+              <p>User Score: {Math.round(details.vote_average * 10)}%</p>
+              <h3>Overview</h3>
+              <p>{details.overview}</p>
+              <h4>Genres</h4>
+              <p>{details.genres.map((genre) => genre.name).join(', ')}</p>
+            </div>
+          </div>
+          <hr />
+          <p>Aditional information</p>
+          <ul className={s.list}>
+            <li>
+              <Link to="cast">Cast</Link>
+            </li>
+            <li>
+              <Link to="reviews">Reviews</Link>
+            </li>
+          </ul>
+          <hr />
+          <Outlet />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default MovieDetailsPage;
