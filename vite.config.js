f22bb5e2a4b61467d92fe9ade99429db94425d5d
PushEax <pusheax.vk@gmail.com>
2025-04-21 21:42:00 +0300

    Fix: correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 const base = process.env.PUBLIC_URL || '/';
 
 // https://vite.dev/config/
-export default defineConfig({
-  base,
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
+// export default defineConfig({
+//   base,
+//   plugins: [react()],
+//   build: {
+//     sourcemap: true,
+//   },
+// });
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: env.PUBLIC_URL || '/',
+    plugins: [react()],
+    build: {
+      sourcemap: true,
+    },
+  };
 });
