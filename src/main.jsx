6efbd26dc49b539b3c157a244b5cce70d8f8fc1b
PushEax <pusheax.vk@gmail.com>
2025-04-21 21:32:48 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppProvider } from './appContext.jsx';
 
 const basename = import.meta.env.PUBLIC_URL || '/';
+console.log(`Basename: ${basename}`);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
