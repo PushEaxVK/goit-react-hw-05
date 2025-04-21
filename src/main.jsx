5f82453e9953f6d4139d91f17fafa9fa4b9f7002
PushEax <pusheax.vk@gmail.com>
2025-04-21 14:15:36 +0300

    add Toaster and BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client';
 import 'modern-normalize';
 import './index.css';
 import App from './App.jsx';
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <Toaster position="bottom-right" />
+    </BrowserRouter>
   </StrictMode>
 );
