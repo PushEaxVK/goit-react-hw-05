783063bd20b8927f4cbde90241a815324494d6e0
PushEax <pusheax.vk@gmail.com>
2025-04-24 11:23:29 +0300

    search bar

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,7 +1,14 @@
 import s from './MoviesPage.module.css';
 
 const MoviesPage = () => {
-  return <div className={s.movies}>MoviesPage page</div>;
+  return (
+    <div className={s.movies}>
+      <form>
+        <input type="text" name="" id="" />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default MoviesPage;
