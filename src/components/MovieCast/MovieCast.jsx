f49c14fa3fbe8affc65f8280883c8d9ac7bf73a7
PushEax <pusheax.vk@gmail.com>
2025-04-24 11:26:18 +0300

    complete MovieCast component

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,7 +1,50 @@
+import { useParams } from 'react-router-dom';
 import s from './MovieCast.module.css';
+import { useFetchData } from '../../hooks/useFetchData';
+import { getCredits } from '../../services/api';
+import Loader from '../Loader/Loader';
+import { useEffect } from 'react';
+import Error from '../Error/Error';
+import { useApp } from '../../appContext';
 
 const MovieCast = () => {
-  return <div className={s.cast}>MovieCast component</div>;
+  const { movieId } = useParams();
+  const { data, loading, error, fetchData, cancel } = useFetchData(getCredits);
+  const { config } = useApp();
+
+  useEffect(() => {
+    fetchData(movieId);
+    return () => cancel();
+  }, [movieId, fetchData, cancel]);
+
+  // useEffect(() => {
+  //   if (data) {
+  //     console.log('Cast', data);
+  //   }
+  // }, [data]);
+
+  return (
+    <div className={s.cast}>
+      {loading && <Loader />}
+      {error && <Error error="Faild to load cast data!" />}
+      {/* MovieCast component {movieId} */}
+      {data && data?.cast && data.cast.length > 0 && (
+        <ul className={s.castList}>
+          {data.cast.map((actor) => (
+            <li key={actor.id}>
+              <img
+                src={`${config.images.secure_base_url}original${actor.profile_path}`}
+                alt={actor.name}
+                width={100}
+              />
+              <p>{actor.name}</p>
+              <p>Character: {actor.character}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default MovieCast;
