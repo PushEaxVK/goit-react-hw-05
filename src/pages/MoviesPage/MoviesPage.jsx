32f1c2bf8945044df24d3a37a75b5a1ab2c13714
PushEax <pusheax.vk@gmail.com>
2025-04-25 12:40:08 +0300

    useSearchParams

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,42 +1,42 @@
 import { useFetchData } from '../../hooks/useFetchData';
 import s from './MoviesPage.module.css';
 import { searchByQuery } from '../../services/api';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import Loader from '../../components/Loader/Loader';
 import Error from '../../components/Error/Error';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import MovieList from '../../components/MovieList/MovieList';
 
 const MoviesPage = () => {
-  const [query, setQuery] = useState('');
-  const { data, loading, error, fetchData, cancel, reset } =
+  const { data, loading, error, fetchData, cancel } =
     useFetchData(searchByQuery);
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const handleSearch = (event) => {
     event.preventDefault();
     const form = event.target;
     const queryString = form.elements.query.value;
-    setQuery(queryString);
+    const urlParams = new URLSearchParams(searchParams);
+    urlParams.set('query', queryString);
+    setSearchParams(urlParams);
   };
 
   useEffect(() => {
-    console.log('Search: ', query);
-    if (query && query.trim()) {
-      // console.log('Search: ', query);
-      fetchData(query);
+    const queryString = searchParams.get('query');
+    if (queryString && queryString.trim()) {
+      fetchData(queryString);
     }
     return cancel;
-  }, [query, cancel, fetchData]);
+  }, [searchParams, cancel, fetchData]);
 
   return (
     <div className={s.movies}>
-      <form onSubmit={handleSearch}>
+      <form onSubmit={handleSearch} className={s.form}>
         <input type="text" name="query" id="query" />
         <button type="submit">Search</button>
       </form>
       {loading && <Loader />}
       {error && <Error error="Faild to load search results!" />}
-      {data && console.log('Data', data)}
       {data?.results && data.results.length && !error > 0 ? (
         <MovieList movies={data.results} />
       ) : (
